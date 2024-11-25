677976f17bb9933aca22ca40cf7d4b3bb0538142
Margubillah HR <115145194+xtelux@users.noreply.github.com>
2024-11-26 02:04:29 +0700

    Create home_page.dart

---
+++ Minggu2/Stateless/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan3/widgets/counter_widget.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: CounterWidget(
+          counter: _counter,
+          description: "Nomor",
+          onClick: () {
+            print('Click');
+            _incrementCounter();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
