262900695a78787e7525db55a10981c3e55db076
Margubillah HR <115145194+xtelux@users.noreply.github.com>
2024-11-26 02:01:02 +0700

    Update home_page.dart

--- Minggu2/pages/home_page.dart
+++ Minggu2/pages/home_page.dart
@@ -1,1 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan3/widgets/counter_widget.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print('Dipanggil hanya satu kali diawal');
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    print('ditutup pada akhir');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: CounterWidget(
+          counter: _counter,
+          description: "Nomor",
+          onClick: () {
+            print('Click');
+            _incrementCounter();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
 
