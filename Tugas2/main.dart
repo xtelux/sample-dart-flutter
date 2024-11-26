57594b2a5b77e8cf4613c6b1c73bd73f9ccbd8c1
Margubillah HR <115145194+xtelux@users.noreply.github.com>
2024-11-26 17:33:06 +0700

    Update main.dart

--- Tugas2/main.dart
+++ Tugas2/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:nusa_code/pages/MainPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Billah Apps',
+      debugShowCheckedModeBanner: false,
+      home: const MainPage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
 
