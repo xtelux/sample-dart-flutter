a1b9c9249baaadb481c92b556af8f41bda79ecc9
Margubillah HR <115145194+xtelux@users.noreply.github.com>
2024-11-26 16:44:06 +0700

    Update main.dart

--- minggu-3/main.dart
+++ minggu-3/main.dart
@@ -1,1 +1,27 @@
 
+import 'package:flutter/material.dart';
+import 'package:nusa_code/pages/halaman_navigasi.dart';
+import 'package:nusa_code/pages/halaman_route.dart';
+import 'package:nusa_code/pages/mainroute.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nusa Code',
+      debugShowCheckedModeBanner: false,
+      home: const Pagenavigasi(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
