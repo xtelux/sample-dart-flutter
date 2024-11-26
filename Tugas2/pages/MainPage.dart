906b1d74d7cb7d91516fe4067648a9fbc0eff91a
Margubillah HR <115145194+xtelux@users.noreply.github.com>
2024-11-26 17:33:56 +0700

    Update MainPage.dart

--- Tugas2/pages/MainPage.dart
+++ Tugas2/pages/MainPage.dart
@@ -1,1 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:nusa_code/pages/HomePage.dart';
+import 'package:nusa_code/pages/ProfilePage.dart';
+import 'package:nusa_code/pages/SettingPage.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+  int SelectIndex = 0;
+
+  final List<Widget> pages = [
+    const HomePage(),
+    const ProfilePage(),
+    const SettingsPage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Billah Apps'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.blueAccent,
+            padding: const EdgeInsets.all(16.0),
+            child: const Text(
+              'Integrasi data',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(child: pages[SelectIndex])
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: SelectIndex,
+          onTap: (index) {
+            setState(() {
+              SelectIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.supervised_user_circle_outlined),
+                label: 'Profile'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Setting'),
+          ]),
+    );
+  }
+}
 
